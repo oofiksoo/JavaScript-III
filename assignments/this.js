77069f4d618fd77ea5561b090847498a5474912a
Joseph Graham <Joseph@Chasingthefiks.net>
2019-10-16 16:31:58 -0600

    MVP Met - Final Commit

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
  * in your own words. explain the four principle for the "this" keyword below.
  *
  * 1. In principal 1 - .this is referenced in a global scope, and is exposed to the top level hiarchy of scope.
- * 2. In principlal 2 - .this is referenced implicitly from its caller on an origin object. This has implyed permissions/scope for actions from its parent/caller object.
+ * 2. In principal 2 - .this is referenced implicitly from its caller on an origin object. This has implyed permissions/scope for actions from its parent/caller object.
  * 3. 
- * 4. direct manipulation of an object by its caller/parent
+ * 4. In principal 3 this is referenced with direct manipulation of an object by its caller/parent. this behaviour can immediatly call, or store the formed call for invoking later.
  *
  * write out a code example of each explanation above
  */
@@ -17,9 +17,9 @@ console.log(this);
 
 // code example for Implicit Binding
 const wrkObj = {
-    message: 'Test',
+    message: 'I like work',
     action: function() {
-        console.log("implicit this: === ", this);
+        console.log("I'll say it implicitly:", this);
         return (`${this.message}`, this);
     }
 };
@@ -43,8 +43,5 @@ console.log(Joseph.doWork());
 
 // code example for Explicit Binding
 
-const work = new jbObj({ title: 'hard worker', wklength: 'a long time in' });
-
-const timeoff = new jbObj({ title: "easy", wklength: "too short" });
-
+const work = new jbObj({ title: 'Hard Worker', wklength: 'Explicit' });
 console.log(Joseph.doWork.call(work));
